9a687cdb5c90ad68962b337fae6fa2b58be32ada
moadisasa-teck <moa.disasa@astu.edu.et>
2025-06-01 13:21:33 +0300

    update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Integrated</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-700">Housing</span>
           </h1>
         </Link>
         <form
