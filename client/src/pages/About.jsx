da945b78b94984ec1969e78b820747b7b328f7af
moadisasa-teck <moa.disasa@astu.edu.et>
2025-06-01 13:00:25 +0300

    update namings

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -5,16 +5,16 @@ export default function About() {
     <div className='py-20 px-4 max-w-6xl mx-auto'>
       <h1 className='text-4xl font-extrabold mb-6 text-slate-800 tracking-tight'>Who We Are</h1>
       <p className='mb-6 text-lg text-slate-700 leading-relaxed'>
-        At <span className='font-semibold text-blue-600'>Integrated Estate</span>, we’re more than just a real estate agency — we’re your partners in finding the place you’ll call home, or making the perfect sale. We specialize in connecting people with properties in the most sought-after neighborhoods, with a smooth, stress-free experience every step of the way.
+        At <span className='font-semibold text-blue-600'>Integrated Estate</span>, we’re more than just a house finding agency — we’re your partners in finding the place you’ll call home, or making the perfect sale. We specialize in connecting people with properties in the most sought-after neighborhoods, with a smooth, stress-free experience every step of the way.
       </p>
       <p className='mb-6 text-lg text-slate-700 leading-relaxed'>
         With years of experience, our passionate team of agents brings local expertise, genuine care, and a personal touch to every interaction. Whether you're buying your first home, selling a beloved property, or searching for the right rental, we’re here to guide and support you.
       </p>
       <p className='mb-6 text-lg text-slate-700 leading-relaxed'>
-        We believe real estate isn’t just about buildings — it’s about people, dreams, and building futures. That’s why our mission is clear: deliver expert advice, exceptional service, and real results that leave a lasting impact.
+        We believe house finding isn’t just about buildings — it’s about people, dreams, and building futures. That’s why our mission is clear: deliver expert advice, exceptional service, and real results that leave a lasting impact.
       </p>
       <p className='text-lg text-slate-700 leading-relaxed'>
-        Discover a better way to move forward — with <span className='font-semibold text-blue-600'>Integrated Estate</span>, where your real estate goals become reality.
+        Discover a better way to move forward — with <span className='font-semibold text-blue-600'>Integrated Estate</span>, where your house finding goals become reality.
       </p>
     </div>
   )
